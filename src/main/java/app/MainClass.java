9ff7be6da5a9a3b7ed95cfd263bbeb67711af7d6
keksen64 <keksen64@gmail.com>
2022-11-20 22:58:21 +0300

    добавлены обработчики для фото и пдф

--- src/main/java/app/MainClass.java
+++ src/main/java/app/MainClass.java
@@ -1,16 +1,7 @@
 package app;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-import java.net.InetSocketAddress;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
+import java.net.InetSocketAddress;
 import com.sun.net.httpserver.HttpServer;
 
 public class MainClass {
@@ -20,129 +11,9 @@ public class MainClass {
         server.createContext("/main", new StatikHTML());
         server.createContext("/blog.css", new StatikcssBlog());
         server.createContext("/bootstrap.min.css", new StatikcssBootstrap());
-        server.setExecutor(null); // creates a default executor
+        server.createContext("/a.png", new StaticImage1());
+        server.createContext("/CV.pdf", new StaticCV());
+        server.setExecutor(java.util.concurrent.Executors.newFixedThreadPool(10));
         server.start();
     }
-
-//    static class MyHandler implements HttpHandler {
-//        @Override
-//        public void handle(HttpExchange t) throws IOException {
-//            String response = "Ответ тестового обработчика";
-//           // String rawString = "Entwickeln Sie mit Vergnügen";
-//            byte[] bytes = response.getBytes(StandardCharsets.UTF_8);
-//            String utf8EncodedString = new String(bytes, StandardCharsets.UTF_8);
-//            t.getResponseHeaders().set("Access-Control-Allow-Origin"," *");
-//            t.getResponseHeaders().set("Content-Type", "text/html; charset=UTF-8");
-//           // t.sendResponseHeaders("Access-Control-Allow-Origin: *");
-//            t.sendResponseHeaders(200, 0);
-//            OutputStream os = t.getResponseBody();
-//            Writer ow = new OutputStreamWriter(os);
-//            ow.write(utf8EncodedString);
-//            ow.close();
-//            os.close();
-//        }
-//    }
-//
-//
-//    static class StatikHTML implements HttpHandler {
-//        @Override
-//        public void handle(HttpExchange httpExchange) throws IOException {
-//
-//            try {
-//                String filePath = "index.html";
-//                byte[] response = Files.readAllBytes(Paths.get(filePath));
-//                httpExchange.setAttribute("Content-Type", "text/html; charset=UTF-8");
-//                httpExchange.getResponseHeaders().set("Access-Control-Allow-Origin", " *");
-//                httpExchange.getResponseHeaders().set("Content-Type", "text/html; charset=UTF-8");
-//                OutputStream outputStream = httpExchange.getResponseBody();
-//                httpExchange.sendResponseHeaders(200, response.length);
-//                outputStream.write(response);
-//                outputStream.flush();
-//                outputStream.close();
-//                httpExchange.getRequestBody().close();
-//            }catch (Exception e){
-//                String responseErr = "ошибка загрузки файла";
-//                byte[] bytesErr = responseErr.getBytes(StandardCharsets.UTF_8);
-//                String utf8EncodedStringErr = new String(bytesErr, StandardCharsets.UTF_8);
-//                httpExchange.getResponseHeaders().set("Access-Control-Allow-Origin", " *");
-//                httpExchange.getResponseHeaders().set("Content-Type", "text/html; charset=UTF-8");
-//                // t.sendResponseHeaders("Access-Control-Allow-Origin: *");
-//                httpExchange.sendResponseHeaders(200, 0);
-//                OutputStream os = httpExchange.getResponseBody();
-//                Writer ow = new OutputStreamWriter(os);
-//                ow.write(utf8EncodedStringErr);
-//                ow.close();
-//                os.close();
-//            }
-//        }
-//    }
-//
-//    static class Statikcss1 implements HttpHandler {
-//        @Override
-//        public void handle(HttpExchange httpExchange) throws IOException {
-//
-//            try {
-//                String filePath = "blog.css";
-//                byte[] response = Files.readAllBytes(Paths.get(filePath));
-//                httpExchange.getResponseHeaders().set("Access-Control-Allow-Origin", " *");
-//                httpExchange.getResponseHeaders().set("Content-Type", "text/css; charset=UTF-8");
-//                httpExchange.setAttribute("Content-Type", "text/css; charset=UTF-8");
-//                OutputStream outputStream = httpExchange.getResponseBody();
-//                httpExchange.sendResponseHeaders(200, response.length);
-//                outputStream.write(response);
-//                outputStream.flush();
-//                outputStream.close();
-//                httpExchange.getRequestBody().close();
-//            }catch (Exception e){
-//                System.out.println(e);
-//                String responseErr = "ошибка загрузки файла";
-//                byte[] bytesErr = responseErr.getBytes(StandardCharsets.UTF_8);
-//                String utf8EncodedStringErr = new String(bytesErr, StandardCharsets.UTF_8);
-//                httpExchange.getResponseHeaders().set("Access-Control-Allow-Origin", " *");
-//                httpExchange.getResponseHeaders().set("Content-Type", "text/html; charset=UTF-8");
-//                // t.sendResponseHeaders("Access-Control-Allow-Origin: *");
-//                httpExchange.sendResponseHeaders(200, 0);
-//                OutputStream os = httpExchange.getResponseBody();
-//                Writer ow = new OutputStreamWriter(os);
-//                ow.write(utf8EncodedStringErr);
-//                ow.close();
-//                os.close();
-//            }
-//        }
-//    }
-//
-//
-//    static class Statikcss2 implements HttpHandler {
-//        @Override
-//        public void handle(HttpExchange httpExchange) throws IOException {
-//            try {
-//                String filePath = "bootstrap.min.css";
-//                // file to byte[], Path
-//                byte[] response = Files.readAllBytes(Paths.get(filePath));
-//                httpExchange.getResponseHeaders().set("Access-Control-Allow-Origin", " *");
-//                httpExchange.getResponseHeaders().set("Content-Type", "text/css; charset=UTF-8");
-//                httpExchange.setAttribute("Content-Type", "text/css; charset=UTF-8");
-//                OutputStream outputStream = httpExchange.getResponseBody();
-//                httpExchange.sendResponseHeaders(200, response.length);
-//                outputStream.write(response);
-//                outputStream.flush();
-//                outputStream.close();
-//                httpExchange.getRequestBody().close();
-//            }catch (Exception e){
-//                System.out.println(e);
-//                String responseErr = "ошибка загрузки файла";
-//                byte[] bytesErr = responseErr.getBytes(StandardCharsets.UTF_8);
-//                String utf8EncodedStringErr = new String(bytesErr, StandardCharsets.UTF_8);
-//                httpExchange.getResponseHeaders().set("Access-Control-Allow-Origin", " *");
-//                httpExchange.getResponseHeaders().set("Content-Type", "text/html; charset=UTF-8");
-//                // t.sendResponseHeaders("Access-Control-Allow-Origin: *");
-//                httpExchange.sendResponseHeaders(200, 0);
-//                OutputStream os = httpExchange.getResponseBody();
-//                Writer ow = new OutputStreamWriter(os);
-//                ow.write(utf8EncodedStringErr);
-//                ow.close();
-//                os.close();
-//            }
-//        }
-//    }
 }
